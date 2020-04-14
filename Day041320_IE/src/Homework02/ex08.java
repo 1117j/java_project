512210708b1e46c1cb9d052f65a81bb6ea5e9a6e
1117j <63032376+1117j@users.noreply.github.com>
2020-04-14 20:52:36 +0900

    Update ex08.java

--- Day041320_IE/src/Homework02/ex08.java
+++ Day041320_IE/src/Homework02/ex08.java
@@ -3,7 +3,7 @@ package Homework02;
 public class ex08 {
 
 	public static void main(String[] args) {
-		// 문자열 참조변수 str이 “yes”일 때 true인 조건식
+//[1] 문자열 참조변수 str이 “yes”일 때 true인 조건식
 		String str = "yes";
 		boolean answer = true;
 		if(str == "yes") {
@@ -11,6 +11,12 @@ public class ex08 {
 		else {
 			System.out.println(!answer);
 		}
+		System.out.println("---------------------------------");
+//[2] 1부터 20까지의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합을 구하시오		
+		for(int q2 = 1; q2 >= 1 && q2 <= 20; q2++) {
+			if 
+		}
+		
 		}	
 	}
 
